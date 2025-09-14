99fc8c2ed06cc799483d1172750a8203470d87b7
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-09-14 18:26:28 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-// pages/_app.js
-import "../styles/globals.css";
-import Layout from "../components/Layout";
+import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <main className={inter.className}>
       <Component {...pageProps} />
-    </Layout>
-  );
+    </main>
+  )
 }
