f4930bf912dd2de8081f93c7524c79785fa60bd5
RaykSchroeder <87695031+RaykSchroeder@users.noreply.github.com>
2025-09-14 17:45:49 +0200

    Update impressum.js

--- pages/impressum.js
+++ pages/impressum.js
@@ -2,7 +2,7 @@
 export default function Impressum() {
   return (
     <div className="max-w-2xl mx-auto space-y-4">
-      <h1 className="text-2xl font-bold">Impressum</h1>
+      <h1 className="text-2xl font-bold">Inpressum</h1>
       <p>Feuerwehr Stadt Geestland - OT Imsum</p>
       <p>Vertreten durch: [TvH, Wehrführer]</p> 
       <p>Verantwortliche Funkübung: [TM, Funkwart]</p>
